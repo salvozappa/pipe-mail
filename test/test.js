d5a197b8933f45b3c764938dc8ff1b2f3205f412
Salvatore Zappalà <salvo.a.zappala@gmail.com>
2018-03-14 10:05:32 +0000

    Extract mock initialisation

--- test/test.js
+++ test/test.js
@@ -4,17 +4,17 @@ const readStandardInput = require('../lib/readStandardInput');
 const EventEmitter = require('events');
 
 describe('readStandardInput', () => {
+
+    const stdinMock = new EventEmitter();
+    stdinMock.setEncoding = td.function('set encoding');
+    stdinMock.read = () => { return 'foo' };
+
     it('Should set the encoding to utf8', () => {
-        const stdinMock = new EventEmitter();
-        stdinMock.setEncoding = td.function('set encoding');
         readStandardInput(stdinMock);
         td.verify(stdinMock.setEncoding('utf8'));
     });
 
     it('Should read the standard input', function () {
-        const stdinMock = new EventEmitter();
-        stdinMock.setEncoding = td.function('set encoding');
-        stdinMock.read = td.function('read');
         setTimeout(() => {
             stdinMock.emit('readable');
             stdinMock.emit('end');
@@ -25,9 +25,6 @@ describe('readStandardInput', () => {
     });
 
     it('Should concatenate multiple chunks', function () {
-        const stdinMock = new EventEmitter();
-        stdinMock.setEncoding = td.function('set encoding');
-        stdinMock.read = () => {return 'foo'};
         setTimeout(() => {
             stdinMock.emit('readable');
             stdinMock.emit('readable');
