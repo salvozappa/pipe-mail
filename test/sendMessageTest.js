0a53d5dcf65d356cb6a3d4ecdd96db37407e80a3
Salvatore Zappalà <salvo.a.zappala@gmail.com>
2018-03-14 16:12:22 +0000

    Improve sendMessage unit testing

--- test/sendMessageTest.js
+++ test/sendMessageTest.js
@@ -18,16 +18,32 @@ describe('sendMessage', () => {
 
     it('Should add the message to the options object', () => {
         sendMessage('This is the message', options, transporterMock);
-        td.verify(transporterMock.sendMail({
-            from: '"Fake e-mail" <fake-email@nodomain.org>',
-            to: 'fake-recipient@nodomain.org',
-            subject: 'Fake subject',
-            text: 'This is the message'
-        }, td.matchers.isA(Function)));
+        td.verify(
+            transporterMock.sendMail(
+                td.matchers.contains({text: 'This is the message'}),
+                td.matchers.anything()
+            )
+        );
     });
 
     it('Should send the message via the nodemailer transporter', () => {
         sendMessage('This is the message', options, transporterMock);
-        td.verify(transporterMock.sendMail(td.matchers.isA(Object), td.matchers.isA(Function)))
+        td.verify(
+            transporterMock.sendMail(
+                td.matchers.isA(Object),
+                td.matchers.isA(Function)
+            )
+        );
     });
+
+    it('Should not pass an invalid option to the transporter', () => {
+        options.foo = 'bar';
+        sendMessage('This is the message', options, transporterMock);
+        td.verify(
+            transporterMock.sendMail(
+                td.matchers.argThat((options) => { return typeof options.foo === 'undefined' }),
+                td.matchers.anything()
+            )
+        );
+    })
 });
