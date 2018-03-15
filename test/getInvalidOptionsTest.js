960eca6a950f1310a021c76bd9ae6c7dab59f433
Salvatore Zappalà <salvo.a.zappala@gmail.com>
2018-03-15 17:39:25 +0000

    Fix broken test

--- test/getInvalidOptionsTest.js
+++ test/getInvalidOptionsTest.js
@@ -55,7 +55,7 @@ describe('getInvalidOptions', () => {
         options = {
             port: 'abc'
         };
-        assert.deepEqual(getInvalidOptions(options), ['abc']);
+        assert.deepEqual(getInvalidOptions(options), ['port']);
     });
 
     it('Should not accept empty credentials', () => {
